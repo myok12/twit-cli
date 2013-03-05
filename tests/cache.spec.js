cbd59b44fce67ea69d6adc829820056c0bdae29e
Trevor <landautrevor@gmail.com>
2013-03-05 09:12:24 -0500

    added file caching util

---
+++ tests/cache.spec.js
@@ -0,0 +1,73 @@
+"use strict";
+
+var fs = require("fs"),
+    path = require("path"),
+    crypto = require("crypto");
+
+var should = require("should");
+
+var Cache = require(path.join("..", "lib", "cache"));
+
+describe("Cache", function () {
+    var dir = path.join(__dirname, "..", ".cache"),
+        key = { foo: "bar" },
+        hash = crypto.createHash("md5").update(JSON.stringify(key)).digest("hex"),
+        filepath = path.join(dir, hash);
+
+    beforeEach(function () {
+        this.cache = new Cache(50);
+        this.now = new Date();
+    });
+
+    afterEach(function () {
+        var files = fs.readdirSync(dir)
+        files.forEach(function (file) {
+            fs.unlinkSync(path.join(dir, file));
+        });
+        fs.rmdirSync(dir);
+    });
+
+    it("should throw an error if `ttl` is not a number", function () {
+        (function () {
+            new Cache();
+        }).should.throw();
+    });
+
+    it("should make a directory to store cached files", function () {
+        fs.existsSync(dir).should.equal(true);
+        fs.statSync(dir).isDirectory().should.equal(true);
+    });
+
+    it("should save data to a file", function () {
+        this.cache.put(key);
+        fs.existsSync(filepath).should.equal(true);
+        fs.statSync(filepath).isFile().should.equal(true);
+    });
+
+    it("should retrieve cached data from a file", function () {
+        this.cache.put(key);
+        var data = this.cache.get(key);
+        data.foo.should.equal("bar");
+    });
+
+    it("should delete a cached file if it is exists", function () {
+        this.cache.put(key);
+        this.cache.del(key);
+        fs.existsSync(filepath).should.not.equal(true);
+    });
+
+    it("should delete a cached file if it is expired", function (done) {
+        this.cache.put(key);
+        setTimeout(function () {
+            var data = this.cache.get(key);
+            should.not.exists(data);
+            done();
+        }.bind(this), 75);
+    });
+
+    it("should clean up all files", function () {
+        this.cache.put(key);
+        Cache.clean();
+        fs.readdirSync(dir).length.should.equal(0);
+    });
+});
